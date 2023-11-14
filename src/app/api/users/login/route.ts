bf8df7cbc5be1b75878c4c2e5b8941797d7c5182
Fareha <hfariha104@gmail.com>
2023-11-14 13:44:31 +0500

    updated

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,7 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 // import path from "path";
 
 connect();
@@ -23,26 +23,26 @@ export async function POST(request: NextRequest) {
 
       if (!validPassword) {
         return NextResponse.json("Invalid Password", { status: 400 });
-      }      
+      }
     }
 
     const tokenData = {
-        id : user._id,
-        username:user.username,
-        email:user.email,
-    }
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
 
     //create token
-    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1h" })
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1h",
+    });
     const response = NextResponse.json({
-        message: "Login Successful!",
-        success: true,
-    })
-    response.cookies.set("token" , token , {httpOnly: true })
+      message: "Login Successful!",
+      success: true,
+    });
+    response.cookies.set("token", token, { httpOnly: true });
 
     return response;
-
-
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,39 +7,35 @@ import { toast } from "react-hot-toast/headless";
 import { Router } from "next/router";
 
 const page = () => {
-  const router= useRouter();
-  const [buttonDisabled , setButtonDisabled]=React.useState(false)
-  const [loading , setLoading]=React.useState(false);
+  const router = useRouter();
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
   const [user, setUser] = React.useState({
     email: "",
     password: "",
-  
   });
   const onLogin = async () => {
-try {
-  setLoading(true);
- const response =  await axios.post("/api/users/login" , user)
- console.log("Login success" , response.data);
-//  toast.success("Login success");
- router.push("/profile");
-
-
-} catch (error:any) {
-  console.log("Login failed", error.message);
-  toast.error(error.message)
-} finally {
-  setLoading(false);
-}
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      //  toast.success("Login success");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
-  useEffect(()=>{
-    if(user.email.length>0 && user.password.length>0 ){
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
       setButtonDisabled(false);
-    }else{
+    } else {
       setButtonDisabled(true);
     }
-    
-  } , [user])
+  }, [user]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -50,7 +46,7 @@ try {
 
       <label htmlFor="email">Email</label>
       <input
-      className="p-2 mb-4 text-black"
+        className="p-2 mb-4 text-black"
         type="email"
         id="email"
         value={user.email}
@@ -60,7 +56,7 @@ try {
       {/* PASSWORD */}
       <label htmlFor="password">Password</label>
       <input
-      className="p-2 mb-4 text-black"
+        className="p-2 mb-4 text-black"
         type="password"
         id="password"
         value={user.password}
@@ -68,8 +64,8 @@ try {
         placeholder="password"
       />
       <button
-      onClick={onLogin}
-      className="p-2 px-5 border border-gray-300 rounded-lg mb-4 hover:bg-white hover:text-black"
+        onClick={onLogin}
+        className="p-2 px-5 border border-gray-300 rounded-lg mb-4 hover:bg-white hover:text-black"
       >
         Login
       </button>
